c85783f2b34bcddcc26dbb9a1e9d908a11484f5d
Hiba Farhat <hiba.farhat@esprit.tn>
2022-04-11 11:26:37 +0100

    initialisation du projet

---
+++ src/main/java/tn/dari/spring/DariTnSpringBootApplication.java
@@ -0,0 +1,22 @@
+package tn.dari.spring;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+
+@SpringBootApplication
+@EnableWebMvc
+public class DariTnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DariTnSpringBootApplication.class, args);
+	}
+
+
+	
+
+}
