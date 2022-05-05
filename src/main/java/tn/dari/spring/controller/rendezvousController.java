a436a88915adc810f29527c10ce3f38969e99571
nadahassen <nada.hassen@esprit.tn>
2022-05-05 07:51:28 +0200

    Fonctions avancés v1.0

--- src/main/java/tn/dari/spring/controller/rendezvousController.java
+++ src/main/java/tn/dari/spring/controller/rendezvousController.java
@@ -3,10 +3,12 @@ package tn.dari.spring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +19,7 @@ import tn.dari.spring.entity.Rendezvous;
 import tn.dari.spring.service.RendezvousService;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/rendezvous")
 @Api(tags="Gestion des rendezvous")
 public class rendezvousController {
@@ -35,7 +38,7 @@ public class rendezvousController {
 		return rendezvousService.addRdv(f,id);
 	}
 	
-	@PostMapping("/updateRdv")
+	@PutMapping("/updateRdv")
 	public Rendezvous updateRdv(@RequestBody Rendezvous f) {
 		return rendezvousService.updateRdv(f);
 	}
--- src/main/java/tn/dari/spring/repository/RendezvousRepository.java
+++ src/main/java/tn/dari/spring/repository/RendezvousRepository.java
@@ -2,11 +2,13 @@ package tn.dari.spring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import tn.dari.spring.entity.Rendezvous;
 
 
 @Repository
+
 public interface RendezvousRepository extends JpaRepository<Rendezvous, Long>{
 
 	
